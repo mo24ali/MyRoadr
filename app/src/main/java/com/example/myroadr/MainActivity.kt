7b1715fcb88ae61b214f317bd24d17f7dcb12758
mo24ali <alichere1234@gmail.com>
2025-05-05 12:20:28 +0100

    initial commit

---
+++ app/src/main/java/com/example/myroadr/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myroadr
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myroadr.databinding.ActivityMainBinding
+import com.example.myroadr.Fragments.EventDiscoveryFragment
+import com.example.myroadr.Fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_events -> replaceFragment(EventDiscoveryFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
