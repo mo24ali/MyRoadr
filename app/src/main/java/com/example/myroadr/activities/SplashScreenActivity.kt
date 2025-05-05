51f32445983156433560fe6116d9fb0c79c445fd
kharrazmohammed <kharraz.mohammed@hotmail.com>
2025-05-05 18:49:16 +0100

    zdt activities packages

--- app/src/main/java/com/example/myroadr/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/myroadr/activities/SplashScreenActivity.kt
@@ -1,12 +1,30 @@
 package com.example.myroadr.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myroadr.MainActivity
 import com.example.myroadr.R
+import com.example.myroadr.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private val SPLASH_DELAY: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateToMainActivity()
+        }, SPLASH_DELAY)
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
